3b7c7843c1b7219ba1a9e6b2a14ac46cbadbe487
2172016AlvinRahmatilasyah <90677192+2172016AlvinRahmatilasyah@users.noreply.github.com>
2023-03-06 13:45:56 +0700

    Update index.php

--- pwl20222/index.php
+++ pwl20222/index.php
@@ -5,14 +5,14 @@
         <meta name="viewport"
               content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Perpustakaan Nasional</title>
+        <title>Perpustakaan Lokal</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
         <div class="container-fluid" >
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <a class="navbar-brand" href="#">Perpustakaan Nasional</a>
+                <a class="navbar-brand" href="#">Perpustakaan Lokal</a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
