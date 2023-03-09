e3ce8256de8f61b69a8e2e1523966d3799c5ea27
Thoriq Hidayansyah <90821052+2172019@users.noreply.github.com>
2023-03-09 23:09:14 +0700

    Delete util_function.php

--- pwl20222/util_function.php
+++
@@ -1,7 +0,0 @@
-<?php
-function createMySQLConnection(){
-    $link = new PDO('mysql:host=localhost;dbname=pwl20222', 'root', '');
-    $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $link ->setAttribute(PDO::ATTR_AUTOCOMMIT, false);
-    return $link;
-}
